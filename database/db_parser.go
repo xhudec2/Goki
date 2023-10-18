2d8cc36be02de61440b555521828f48e9582ee91
xhudec2 <lukyhudec@gmail.com>
2023-10-19 00:56:50 +0200

    added functions for updating db

--- database/db_parser.go
+++ database/db_parser.go
@@ -9,10 +9,10 @@ import (
 )
 
 const CARD_DELIMITER = "\x1f"
+const attributes = "id, nid, did, queue, due, ivl, reps, lapses, left"
 
 type Name string
-type Id string
-type Decks map[Id]Name
+type Decks map[int]Name
 
 type Back string
 type Front string
@@ -23,7 +23,6 @@ type Card struct {
 }
 
 type Cards map[Id]Card
-type Card_ids []string
 
 func Open_db(filepath string) (db *sql.DB, err error) {
 	db, err = sql.Open("sqlite3", filepath)
@@ -44,18 +43,19 @@ func Get_decks(db *sql.DB) (decks Decks, err error) {
 
 	decks = make(Decks, 4)
 	for rows.Next() {
-		var id, name string
+		var id int
+		var name string
 		err = rows.Scan(&id, &name)
 		if err != nil {
 			fmt.Println("Error scanning decks rows:", err)
 			return
 		}
-		decks[Id(id)] = Name(name)
+		decks[id] = Name(name)
 	}
 	return
 }
 
-func Get_Cards(deck_id Id, db *sql.DB) (card_ids Card_ids, cards Cards, err error) {
+func Get_Cards(deck_id Id, db *sql.DB) (cards Cards, err error) {
 	rows, err := db.Query(fmt.Sprintf("SELECT id FROM cards WHERE did == %s", deck_id))
 	if err != nil {
 		fmt.Println("Error querying DB cards:", err)
@@ -66,20 +66,26 @@ func Get_Cards(deck_id Id, db *sql.DB) (card_ids Card_ids, cards Cards, err erro
 	cards = make(Cards, 32)
 	card_ids = make(Card_ids, 0, 32)
 	for rows.Next() {
-		var card_id string
-		err = rows.Scan(&card_id)
+		new_card := Card{}
+		err = rows.Scan(
+			&new_card.Id, &new_card.Nid, &new_card.Did, &new_card.Q_type, &new_card.Due,
+			&new_card.Ivl, &new_card.Reps, &new_card.Lapses, &new_card.Left)
+
 		if err != nil {
 			fmt.Println("Error scanning cards rows:", err)
 			return
 		}
 		cards[Id(card_id)] = Card{}
-		card_ids = append(card_ids, card_id)
 	}
 
 	return
 }
 
-func Get_notes(card_ids Card_ids, cards Cards, db *sql.DB) (err error) {
+func Get_notes(cards Cards, db *sql.DB) (err error) {
+	card_ids := make([]string, 0, len(cards))
+	for card := range cards {
+		card_ids = append(card_ids, string(card))
+	}
 	rows, err := db.Query(fmt.Sprintf("SELECT id, flds FROM notes WHERE id IN (%s)", strings.Join(card_ids, ",")))
 	if err != nil {
 		fmt.Println("Error querying DB notes:", err)
@@ -88,14 +94,20 @@ func Get_notes(card_ids Card_ids, cards Cards, db *sql.DB) (err error) {
 	defer rows.Close()
 
 	for rows.Next() {
-		var card_id, flds string
+		var card_id int
+		var flds string
 		err = rows.Scan(&card_id, &flds)
 		if err != nil {
 			fmt.Println("Error scanning notes rows:", err)
 			return
 		}
-		card := strings.Split(flds, CARD_DELIMITER)
-		cards[Id(card_id)] = Card{Front: Front(card[0]), Back: Back(card[1])}
+		//have to rewrite this, again..
+
+		//card := strings.Split(flds, CARD_DELIMITER)
+		//fmt.Println(cards[card_id].Field)
+		//updated_card := cards[card_id]
+		//updated_card.Field = Field{Front: card[0], Back: card[1]}
+		//cards[card_id] = updated_card
 	}
 	return
 }
