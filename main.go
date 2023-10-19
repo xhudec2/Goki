625aa02d943d23223e971c5b222fcea31eaf13ff
xhudec2 <lukyhudec@gmail.com>
2023-10-20 00:21:14 +0200

    added functionality for importing databases

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 )
 
 func main() {
-
 	db, err := database.Open_db("Anki2/User 1/collection.anki2")
 	if err != nil {
 		return
 	}
 	defer db.Close()
+	database.Import_db(db, "Anki2/User 1/collection_copy.anki2")
 
 }
