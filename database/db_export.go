e0ba3cf4f2136e28ae82c912c50c2693fbdbf613
xhudec2 <lukyhudec@gmail.com>
2023-10-31 00:01:52 +0100

    pain...

--- database/db_export.go
+++ database/db_export.go
@@ -1,29 +1,73 @@
 package database
 
 import (
-	"database/sql"
+	"archive/zip"
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 )
 
-const TEMPLATE_PATH = "db_media/template.anki2"
-const EXPORT_NAME = "export.anki2"
+const DB_TEMPLATE_PATH = "db_media/template.anki2"
+const MEDIA_TEMPLATE_PATH = "db_media/media.template.db2"
+const MAIN_DB = "Anki2/User 1/collection.anki2"
 
-func Export_db(db *sql.DB, decks []int) (err error) {
-	err = create_empty_template(db)
+func Export_db(decks string, name string) (err error) {
+	apkg_export, err := os.Create(name + ".apkg")
 	if err != nil {
-		fmt.Println("err occured: ", err)
+		fmt.Println("Error creating .apkg file:", err)
 		return
 	}
-	fmt.Println("template copied")
+	defer apkg_export.Close()
+
+	zip_writer := zip.NewWriter(apkg_export)
+	if err != nil {
+		panic(err)
+	}
+
+	defer zip_writer.Close()
+
+	write_data(decks, zip_writer, name)
+
 	return
 }
 
-func create_empty_template(db *sql.DB) (err error) {
-	cmd := exec.Command("cp", TEMPLATE_PATH, EXPORT_NAME)
+func Create_empty_template(name string) (err error) {
+	cmd := exec.Command("cp", DB_TEMPLATE_PATH, name+".anki2")
 	if err = cmd.Run(); err != nil {
 		fmt.Println("Error creating template", err)
 		return
+	}
+	return
+}
+
+func write_data(decks string, zip_writer *zip.Writer, name string) (err error) {
+	collection, err := os.Open(DB_TEMPLATE_PATH)
+	if err != nil {
+		panic(err)
+	}
+	defer collection.Close()
+
+	temp_writer, err := zip_writer.Create(name + ".anki2")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := io.Copy(temp_writer, collection); err != nil {
+		panic(err)
+	}
+
+	media, err := os.Open(MEDIA_TEMPLATE_PATH)
+	if err != nil {
+		panic(err)
+	}
+	defer media.Close()
+
+	temp_writer, err = zip_writer.Create(name + ".media.db2")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := io.Copy(temp_writer, media); err != nil {
+		panic(err)
 	}
 	return
 }
--- database/db_import.go
+++ database/db_import.go
@@ -3,35 +3,66 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/mattn/go-sqlite3"
 )
 
-func insert(main_db *sql.DB, table string) (err error) {
-	_, err = main_db.Exec(fmt.Sprintf("INSERT INTO %s SELECT * FROM source_db.%s", table, table))
+const VER = 18
+
+func insert(dest *sql.DB, table string, where_clause string) (err error) {
+	_, err = dest.Exec(fmt.Sprintf("INSERT INTO %s SELECT * FROM source_db.%s %s", table, table, where_clause))
 	if sqliteErr, ok := err.(sqlite3.Error); ok {
 		if sqliteErr.Code != 19 || sqliteErr.ExtendedCode != 1555 {
-			fmt.Printf("insert %s failed: %v", table, err)
+			fmt.Printf("insert %s failed: %v %s", table, err, where_clause)
 			return
 		}
 	}
 	return nil
 }
 
-func Import_db(main_db *sql.DB, source string) (err error) {
-	_, err = main_db.Exec(fmt.Sprintf("ATTACH DATABASE '%s' AS source_db", source))
+func Import_db(dest *sql.DB, source string) (err error) {
+	_, err = dest.Exec(fmt.Sprintf("ATTACH DATABASE '%s' AS source_db", source))
 	if err != nil {
 		fmt.Println("attach failed: ", err)
 		return
 	}
 
-	if err = insert(main_db, "cards"); err != nil {
+	if err = insert(dest, "cards", ""); err != nil {
 		return
 	}
 
-	if err = insert(main_db, "notes"); err != nil {
+	if err = insert(dest, "notes", ""); err != nil {
 		return
 	}
-	_, err = main_db.Exec("DETACH DATABASE source_db")
+	_, err = dest.Exec("DETACH DATABASE source_db")
+	return
+}
+
+func Create_export_db(dest *sql.DB, source string, decks string) (err error) {
+	_, err = dest.Exec(fmt.Sprintf("ATTACH DATABASE '%s' AS source_db", source))
+	if err != nil {
+		fmt.Println("attach failed: ", err)
+		return
+	}
+
+	if err = insert(dest, "cards", fmt.Sprintf("WHERE did IN (%s)", decks)); err != nil {
+		return
+	}
+
+	if err = insert(dest, "decks", fmt.Sprintf("WHERE id IN (%s)", decks)); err != nil {
+		return
+	}
+
+	if err = insert(dest, "notes", "WHERE id IN (SELECT id FROM cards)"); err != nil {
+		return
+	}
+	curr_time := time.Now().UnixMilli()
+	_, err = dest.Exec(fmt.Sprintf("INSERT INTO col VALUES (1, %d, %d, %d, 0, 0, 0, '', '', '', '' , '')", curr_time, curr_time, VER))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = dest.Exec("DETACH DATABASE source_db")
 	return
 }
