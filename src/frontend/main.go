fdca828b16c30a6a912710c7db889fe1307b6335
Lukáš Hudec <lukyhudec@gmail.com>
2023-11-16 16:39:14 +0100

    deck selection

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	. "src/frontend/window"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
 )
 
 func main() {
-	Draw()
+	app := app.New()
+	app.Settings().SetTheme(&Theme{})
+
+	appWindow := app.NewWindow("GOki")
+	appWindow.Resize(fyne.NewSize(800, 600))
+	appWindow.CenterOnScreen()
+
+	//d1 := Deck{ID: 1, Name: "Karaktärsdrag", New: 5, Learn: 10, Due: 15}
+	//d2 := Deck{ID: 2, Name: "Frisk som en fisk", New: 1, Learn: 1, Due: 1}
+	//d3 := Deck{ID: 3, Name: "Människokroppen", New: 0, Learn: 0, Due: 0}
+	//d4 := Deck{ID: 4, Name: "Utseende", New: 0, Learn: 0, Due: 5}
+	d1 := Deck{ID: 1, Name: "Deck 1", New: 5, Learn: 10, Due: 15}
+	d2 := Deck{ID: 2, Name: "Deck 2", New: 1, Learn: 1, Due: 1}
+	d3 := Deck{ID: 3, Name: "Deck 3", New: 0, Learn: 0, Due: 0}
+	d4 := Deck{ID: 4, Name: "Deck 4", New: 0, Learn: 0, Due: 5}
+	decks := []Deck{
+		d1, d2, d3, d4,
+	}
+	appData := AppData{
+		App:    app,
+		Window: appWindow,
+		Decks:  &decks,
+	}
+	Draw(&appData)
+	appWindow.ShowAndRun()
 }
--- src/frontend/window/window.go
+++ src/frontend/window/window.go
@@ -1,59 +1,54 @@
 package window
 
 import (
+	"fmt"
 	"image/color"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
-func Draw() {
-	app := app.New()
-	app.Settings().SetTheme(&Theme{})
-
-	appWindow := app.NewWindow("GOki")
-	appWindow.Resize(fyne.NewSize(800, 600))
-
-	appWindow.SetContent(
-		container.NewVBox(
-			MenuButtons(),
-			layout.NewSpacer(),
-			Decks(),
-			layout.NewSpacer(),
-		),
-	)
-
-	appWindow.CenterOnScreen()
-	appWindow.ShowAndRun()
+type Deck struct {
+	ID    int
+	Name  string
+	New   int
+	Learn int
+	Due   int
 }
 
-func Decks() *fyne.Container {
+type AppData struct {
+	App    fyne.App
+	Window fyne.Window
+	Decks  *[]Deck
+}
 
-	canv := canvas.NewRectangle(color.RGBA{65, 65, 65, 255})
-	canv.CornerRadius = 10
-	canv.SetMinSize(fyne.NewSize(400, 300))
-
-	title := container.NewHBox(
-		widget.NewLabel("Deck Name"),
-		layout.NewSpacer(),
-		container.NewGridWithColumns(
-			3,
-			widget.NewLabel("New"),
-			widget.NewLabel("Learn"),
-			widget.NewLabel("Due"),
+func Draw(data *AppData) {
+	data.Window.SetContent(
+		container.NewVBox(
+			MenuButtons(data),
+			layout.NewSpacer(),
+			Decks(data),
+			layout.NewSpacer(),
 		),
 	)
+	data.Window.Show()
+}
+
+func Decks(data *AppData) *fyne.Container {
+
+	canv := canvas.NewRectangle(color.RGBA{65, 65, 65, 255})
+	canv.CornerRadius = 15
+	canv.SetMinSize(fyne.NewSize(400, 300))
 
 	stack := container.NewStack(
 		canv,
 		container.NewPadded(
 			container.NewVBox(
-				title,
-				decksList(),
+				title(),
+				decksList(data),
 			),
 		),
 	)
@@ -63,36 +58,42 @@ func Decks() *fyne.Container {
 	)
 }
 
-func decksList() *fyne.Container {
+func title() *fyne.Container {
+	return container.NewHBox(
+		layout.NewSpacer(),
+		widget.NewLabel("Deck"),
+		layout.NewSpacer(),
+		container.NewGridWithColumns(
+			3,
+			widget.NewLabel("New"),
+			widget.NewLabel("Learn"),
+			widget.NewLabel("Due"),
+		),
+	)
+}
 
-	var data = []string{
-		"list", "list", "list", "list", "list", // "list",
-		//"list", "list", "list", "list", "list", "list",
-	}
+func decksList(data *AppData) *fyne.Container {
+
 	buttons := container.NewVBox()
-	for _, label := range data {
+	for index := range *data.Decks {
 		canv := canvas.NewRectangle(color.RGBA{0, 0, 0, 0})
 		canv.SetMinSize(fyne.NewSize(200, 10))
 
 		cardData := container.NewWithoutLayout(
-			widget.NewLabel("0"),
-			widget.NewLabel("0"),
-			widget.NewLabel("0"),
+			widget.NewLabel(fmt.Sprintf("%d", (*data.Decks)[index].New)),
+			widget.NewLabel(fmt.Sprintf("%d", (*data.Decks)[index].Learn)),
+			widget.NewLabel(fmt.Sprintf("%d", (*data.Decks)[index].Due)),
 		)
 		cardData.Objects[0].Move(fyne.NewPos(40, 0))
-		cardData.Objects[1].Move(fyne.NewPos(110, 0))
-		cardData.Objects[2].Move(fyne.NewPos(165, 0))
-		bg := container.NewStack(
+		cardData.Objects[1].Move(fyne.NewPos(105, 0))
+		cardData.Objects[2].Move(fyne.NewPos(160, 0))
+		background := container.NewStack(
 			canv,
 			cardData,
 		)
-		deck := container.NewHBox(
-			widget.NewButton(label, nil),
-			layout.NewSpacer(),
-			bg,
-		)
-		buttons.Add(deck)
+		buttons.Add(makeDeckButton(data, index, background))
 	}
+
 	list := container.NewScroll(buttons)
 	list.SetMinSize(fyne.NewSize(400, 250))
 	padded := container.NewPadded(list)
@@ -103,12 +104,23 @@ func decksList() *fyne.Container {
 	)
 }
 
-func MenuButtons() *fyne.Container {
+func makeDeckButton(data *AppData, index int, background *fyne.Container) *fyne.Container {
+	return container.NewHBox(
+		widget.NewButton(
+			(*data.Decks)[index].Name,
+			func() {
+				StudyDeck(data, index)
+			},
+		),
+		layout.NewSpacer(),
+		background,
+	)
+}
 
-	decksButton := widget.NewButton("Decks", nil)
-	cardsButton := widget.NewButton("Add Card", nil)
-	addDeckButton := widget.NewButton("Create Deck", nil)
-
+func MenuButtons(data *AppData) *fyne.Container {
+	decksButton := widget.NewButton("Decks", func() { Draw(data) })
+	cardsButton := widget.NewButton("Add Card", func() { AddCard(data.App) })
+	addDeckButton := widget.NewButton("Create Deck", func() { CreateDeck(data.App) })
 	return container.NewHBox(
 		layout.NewSpacer(),
 		container.NewGridWithColumns(
@@ -119,4 +131,86 @@ func MenuButtons() *fyne.Container {
 		),
 		layout.NewSpacer(),
 	)
+}
+
+func AddCard(app fyne.App) {
+	w := app.NewWindow("Add Card")
+	w.Resize(fyne.NewSize(600, 450))
+	w.CenterOnScreen()
+	w.SetContent(
+		container.NewVBox(
+			widget.NewLabel("Add Card"),
+		),
+	)
+	w.Show()
+}
+
+func CreateDeck(app fyne.App) {
+	w := app.NewWindow("Create Deck")
+	w.Resize(fyne.NewSize(600, 450))
+	w.CenterOnScreen()
+	w.SetContent(
+		container.NewVBox(
+			widget.NewLabel("Create Deck"),
+		),
+	)
+	w.Show()
+}
+
+func DrawDeck(name string) *fyne.Container {
+	return container.NewVBox(
+		layout.NewSpacer(),
+		container.NewHBox(
+			layout.NewSpacer(),
+			widget.NewLabel(name),
+			layout.NewSpacer(),
+		),
+		layout.NewSpacer(),
+	)
+}
+
+func StudyDeck(data *AppData, index int) {
+	cont := container.NewGridWithRows(
+		3,
+		container.NewHBox(
+			widget.NewLabel("New"),
+			layout.NewSpacer(),
+			widget.NewLabel(fmt.Sprintf("%d", (*data.Decks)[index].New)),
+		),
+		container.NewHBox(
+			widget.NewLabel("Learn"),
+			layout.NewSpacer(),
+			widget.NewLabel(fmt.Sprintf("%d", (*data.Decks)[index].Learn)),
+		),
+		container.NewHBox(
+			widget.NewLabel("Due"),
+			layout.NewSpacer(),
+			widget.NewLabel(fmt.Sprintf("%d", (*data.Decks)[index].Due)),
+		),
+	)
+	canv := canvas.NewRectangle(color.RGBA{0, 0, 0, 0})
+	canv.SetMinSize(fyne.NewSize(300, 10))
+
+	data.Window.SetContent(
+		container.NewVBox(
+			MenuButtons(data),
+			layout.NewSpacer(),
+			container.NewCenter(
+				container.NewStack(
+					canv,
+					container.NewHBox(
+						cont,
+						layout.NewSpacer(),
+						DrawDeck((*data.Decks)[index].Name),
+					),
+				),
+			),
+			layout.NewSpacer(),
+			container.NewCenter(
+				widget.NewButton("Study Deck", nil),
+			),
+			layout.NewSpacer(),
+		),
+	)
+	data.Window.Show()
 }
